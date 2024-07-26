d6bc3f0ef46401e13a1242c179db1ad42402c302
Josue Gonzalez <josuegr834@gmail.com>
2024-07-26 19:55:11 -0300

    Modifique el componente de Switch

--- src/components/Header/Switch.jsx
+++ src/components/Header/Switch.jsx
@@ -1,8 +1,8 @@
 export default function SwitchButton() {
   return (
-    <label className="inline-flex items-center relative">
+    <label className="inline-flex items-center relative ">
       <input className="peer hidden" id="toggle" type="checkbox" />
-      <div className="relative w-[110px] h-[50px] bg-white peer-checked:bg-zinc-500 rounded-full after:absolute after:content-[''] after:w-[40px] after:h-[40px] after:bg-gradient-to-r from-orange-500 to-yellow-400 peer-checked:after:from-zinc-900 peer-checked:after:to-zinc-900 after:rounded-full after:top-[5px] after:left-[5px] active:after:w-[50px] peer-checked:after:left-[105px] peer-checked:after:translate-x-[-100%] shadow-sm duration-300 after:duration-300 after:shadow-md"></div>
+      <div className="relative w-[90px] h-[30px] bg-white peer-checked:bg-zinc-500 rounded-full after:absolute after:content-[''] after:w-[30px] after:h-[30px] after:bg-gradient-to-r from-orange-500 to-yellow-400 peer-checked:after:from-zinc-900 peer-checked:after:to-zinc-900 after:rounded-full after:top-[0px] after:left-[10px] active:after:w-[50px] peer-checked:after:left-[80px] peer-checked:after:translate-x-[-100%] shadow-sm duration-300 after:duration-300 after:shadow-md"></div>
       <svg
         height="0"
         width="100"
